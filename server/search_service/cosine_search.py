3839862d51e7ff3208cc2005d69fee9d53932ba9
Tamal Mallick <tamalmallick.work@gmail.com>
2025-09-22 11:59:07 +0530

    core: complete search service (V3 phase 1.3)
    - Shifted Pinecone client declaration to module start, reducing query time from ~3s to ~0.6s
    - Completed core features of V3 (phase 1.3)
    - Note: still need to handle Pinecone client failure cases

--- server/search_service/cosine_search.py
+++ server/search_service/cosine_search.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict, Any
 from functools import partial
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
@@ -16,6 +16,10 @@ from config import (
 SEMAPHORE = asyncio.Semaphore(MAX_CONCURRENT_TASKS)
 EXECUTOR = ThreadPoolExecutor(max_workers=MAX_CONCURRENT_TASKS)
 
+PC_CLIENT = Pinecone(api_key=PINECONE_KEY, environment=PINECONE_ENVIRONMENT)
+INDEX = PC_CLIENT.Index(PINECONE_INDEX)
+
+
 def _search_pinecone_db(
     query_vector: List[float],
     api_key: str = PINECONE_KEY,
@@ -23,12 +27,12 @@ def _search_pinecone_db(
     index_name: str = PINECONE_INDEX,
     namespace: str = PINECONE_NAMESPACE,
     top_k: int = int(PINECONE_RESPONSE_COUNT)
-) -> List[dict] :
+) -> List[Dict[str, Any]] :
+    """Search in pinecone and return raw matches"""
     try:
-        print("Req: ", type(query_vector))
-        client=Pinecone(api_key=api_key,environment=environment)
-        index=client.Index(index_name)
-        result = index.query(
+        # client=Pinecone(api_key=api_key,environment=environment)
+        # index=client.Index(index_name)
+        result = INDEX.query(
             namespace= namespace,
             vector= query_vector,
             top_k= top_k,
@@ -40,8 +44,39 @@ def _search_pinecone_db(
     except Exception as e:
         raise RuntimeError(f"Pinecone query failed")
 
+def _process_matches(matches: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+    """ Takes raw pinecone matches , keep unique title pages and refactor page in dict and return list of such ditcs"""
+    seen_titles, cleaned_results = set(), []
+
+    for page in matches:
+        metadata = page.get("metadata", {})
+        title = metadata.get("title")
+
+        if not title or title in seen_titles:
+            continue
+        seen_titles.add(title)
+
+        link = metadata.get("link")
+        desc_raw = metadata.get("desc", "")
+        desc = " ".join(desc_raw.split("|@|") if desc_raw else [])
+
+        h1 = metadata.get("h1", "")
+        h2 = metadata.get("h2", "")
+        
+        cleaned_results.append({
+            "title": title,
+            "link": link,
+            "desc": desc,
+            "h1": h1.split("|@|") if h1 else [],
+            "h2": h2.split("|@|") if h2 else [],
+        })
+
+    return cleaned_results
+
 async def search_pinecone(query_vector: List[float]) -> List[dict]:
+    """Leverages pinecone search and refactor leveraging dedicated function """
     async with SEMAPHORE:
         event_loop = asyncio.get_running_loop()
         function = partial(_search_pinecone_db, query_vector= query_vector) 
-        return await event_loop.run_in_executor(executor=EXECUTOR, func=function)
+        matches = await event_loop.run_in_executor(executor=EXECUTOR, func=function)
+        return _process_matches(matches)
--- server/search_service/main.py
+++ server/search_service/main.py
@@ -1,14 +1,20 @@
-from typing import List
+from typing import List, Dict, Any
 from fastapi import FastAPI
 from pydantic import BaseModel
 from cosine_search import search_pinecone
 
 app = FastAPI()
 
-@app.post("/search")
-async def search_vector(query_vector: str) -> dict:
-    res= await search_pinecone(query_vector= query_vector)
-    return {"pinecone_credential": res}
+class SearchRequest(BaseModel):
+    query_vector: List[float]
+
+@app.post("/search", response_model= List[Dict[str, Any]])
+async def search_vector(request: SearchRequest) -> List[Dict[str, Any]]:
+    """
+    Accepts a vector and returns list of dictionary each ahving page metadata.
+    """
+    res= await search_pinecone(query_vector= request.query_vector)
+    return res
 
 if __name__== "__main__":
     import uvicorn
