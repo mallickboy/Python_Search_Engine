4c393947737962f79e40b92bffa96bfc769580f9
Tamal Mallick <tamalmallick.work@gmail.com>
2025-09-27 03:45:51 +0530

    core: Small progress in gateway service
     - Recovered the icon for the site
     - Served a basic html file as hompage
     - Main implementation are still pending

--- server/gateway_service/main.py
+++ server/gateway_service/main.py
@@ -1,14 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates 
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+templates= Jinja2Templates(directory="templates")
+
 @app.get('/', response_class = HTMLResponse)
-async def home():
-    return "<h1>Hi there</h1>"
+async def home(req: Request):
+    return templates.TemplateResponse("ignore.html", {"request": req} )
 
 if __name__ == "__main__":
     import uvicorn
